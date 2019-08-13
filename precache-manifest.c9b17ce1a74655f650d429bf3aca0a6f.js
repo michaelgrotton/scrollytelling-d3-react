c0d9c6745e80d6351ab1154501f77842351b88a6
frankiesastronautfund <grotton.2@buckeyemail.osu.edu>
2019-08-12 23:40:06 -0400

    Updates

---
+++ precache-manifest.c9b17ce1a74655f650d429bf3aca0a6f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de58f7aa4e78f68290dbfab28ad4dac3",
+    "url": "/scrollytelling-d3-react/index.html"
+  },
+  {
+    "revision": "da86e758f8d9a0a6e50f",
+    "url": "/scrollytelling-d3-react/static/js/2.1c499b88.chunk.js"
+  },
+  {
+    "revision": "3177c9c2dc43e32d64ea",
+    "url": "/scrollytelling-d3-react/static/js/main.37975831.chunk.js"
+  },
+  {
+    "revision": "7fed1480b38be312bcdd",
+    "url": "/scrollytelling-d3-react/static/js/runtime~main.e2f8598a.js"
+  }
+]);
