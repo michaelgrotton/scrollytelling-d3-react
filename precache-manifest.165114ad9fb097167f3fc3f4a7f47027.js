909edd84817af5dc0d9ccc97931bd84419468302
frankiesastronautfund <grotton.2@buckeyemail.osu.edu>
2019-08-12 23:52:48 -0400

    Updates

---
+++ precache-manifest.165114ad9fb097167f3fc3f4a7f47027.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c0775aa70b5e5370dc77e0593efbd3",
+    "url": "/scrollytelling-d3-react/index.html"
+  },
+  {
+    "revision": "972bd98325c9ca0d0aac",
+    "url": "/scrollytelling-d3-react/static/js/2.b8c885b7.chunk.js"
+  },
+  {
+    "revision": "f42832f30fb60c87fab9",
+    "url": "/scrollytelling-d3-react/static/js/main.d69d594a.chunk.js"
+  },
+  {
+    "revision": "7fed1480b38be312bcdd",
+    "url": "/scrollytelling-d3-react/static/js/runtime~main.e2f8598a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scrollytelling-d3-react/precache-manifest.a7a6b933d94e6eb95cb76b252b5c46c0.js"
+  "/scrollytelling-d3-react/precache-manifest.165114ad9fb097167f3fc3f4a7f47027.js"
 );
 
 self.addEventListener('message', (event) => {
