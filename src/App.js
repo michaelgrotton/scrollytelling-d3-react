d940b8288cae1e053831e105d69a4c100ddb3242
frankiesastronautfund <grotton.2@buckeyemail.osu.edu>
2019-08-12 23:34:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,157 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import injectSheet from "react-jss";
+import { Scrollama, Step } from "react-scrollama";
+import nyc from "./nyc.js"
+import sf from "./sf.js"
+import am from "./am.js"
+import BarChart from "./BarChart.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+  graphic: {
+    flexBasis:"60%",
+    height:"300px",
+    borderRadius:"5px",
+    position: "sticky",
+    top:"250px",
+    fontSize:"40px"
+  },
+  description: {
+    height:"600px",
+    textAlign:"center",
+    padding:"50px 50px",
+    fontSize:"20px"
+  },
+  container: {
+    display:"flex",
+    justifyContent:"space-around",
+    flexDirection:"column"
+  },
+  step:{
+    height:"100px",
+    width:"100%",
+    backgroundColor:"whitesmoke",
+    border:"2px solid grey",
+    borderRadius:"5px",
+    marginBottom:"500px",
+    fontSize:"25px",
+    textAlign:"center",
+    lineHeight:"100px",
+    zIndex:1,
+    opacity:".8"
+  },
+  scroller: {
+    flexBasis:"30%"
+  },
+  offset: {
+    borderTop:"2px dashed black",
+    marginTop:"32vh",
+    position:"fixed",
+    zIndex:"1",
+    width:"100%"
+  },
+  title: {
+    margin:"20px 0",
+    padding:"0",
+    fontSize:"25px"
+  },
+  "@media (min-width: 768px)": {
+    container: {
+      flexDirection:"row"
+    },
+    step: {
+      '&:last-child': {
+        marginBottom: "200px",
+      }
+    },
+    description: {
+      padding:"100px 250px"
+    }
+  }
 }
 
-export default App;
+const cities = ["nyc", "sf", "am"]
+const cityNames = {"nyc": "New York City", "sf": "San Francisco", "am": "Amsterdam", "": ""}
+
+class App extends React.Component {
+  state = {
+    temps:{},
+    city: "",
+    screenWidth: 0,
+    screenHeight: 0
+  }
+
+  componentDidMount() {
+      sf.forEach(day => (day.date = new Date(day.date)));
+      nyc.forEach(day => (day.date = new Date(day.date)));
+      am.forEach(day => (day.date = new Date(day.date)));
+      this.setState({ temps: { sf, nyc, am } });
+      window.addEventListener('resize', this.onResize, false)
+      this.onResize()
+  }
+
+  onResize = () => {
+    let screenWidth = window.innerWidth
+    let screenHeight = window.innerHeight
+
+    if( screenWidth > 768 ) {
+      screenWidth = screenWidth * .55;
+    } else {
+      screenWidth = screenWidth * .90;
+    }
+
+    this.setState({screenWidth, screenHeight})
+  }
+
+  onStepEnter = ({ element, data, direction }) => {
+    this.setState({ city: data });
+    element.style.backgroundColor = 'lightgoldenrodyellow';
+    element.style.border = '2px solid gold';
+  }
+
+  onStepExit = ({ element, data, direction }) => {
+    if(data === "nyc" && direction === "up") {
+      this.setState({ city: ""});
+    }
+    element.style.backgroundColor = 'whitesmoke';
+    element.style.border = '2px solid grey';
+  }
+
+  render() {
+    const { classes } = this.props
+    const { city, screenWidth, screenHeight } = this.state
+
+    return (
+      <div>
+        <div className={classes.offset}>trigger line</div>
+        <div className={classes.description}>
+          Scrollytelling is a technique used to make changes to a graphic or other ui component as a reader scrolls down a page.
+          In this example, I use <a href="https://github.com/jsonkao/react-scrollama">Jason Kao's</a> React interface of the Scrollama library by <a href="https://github.com/russellgoldenberg/scrollama">Russell Goldenberg</a>, to alter a line graph
+          of weather data built with D3.js as you scroll. Try it out!
+        </div>
+        <div className={classes.container}>
+          <div className={classes.graphic}>
+            <p className={classes.title}>2018 Weather in: <span style={{color:"#1aa3ff", padding:"3px", borderRadius:"2px"}}>{cityNames[city]}</span></p>
+            <BarChart width={screenWidth} height={screenHeight} data={city ? this.state.temps[city] : {} } />
+          </div>
+          <div className={classes.scroller}>
+            <Scrollama onStepEnter={this.onStepEnter} offset={.33} onStepExit={this.onStepExit}>
+              {cities.map(city => {
+                return(
+                  <Step data={city} key={city}>
+                    <div className={classes.step}>
+                      {cityNames[city]} weather
+                    </div>
+                  </Step>
+                )
+              })}
+            </Scrollama>
+          </div>
+        </div>
+
+        <div style={{height:"600px"}}></div>
+      </div>
+    );
+  }
+}
+
+export default injectSheet(styles)(App);
---
+++ src/BarChart.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import * as d3 from "d3";
+const margin = { top: 20, right: 5, bottom: 20, left: 35 };
+const height = 400;
+
+class BarChart extends Component {
+  state = {
+    bars: []
+  };
+
+  xAxis = d3.axisBottom().tickFormat(d3.timeFormat("%b"));
+  yAxis = d3.axisLeft().tickFormat(d => `${d}℉`);
+
+
+  componentDidMount() {
+    this.createChart()
+  }
+
+  componentDidUpdate() {
+    this.createChart()
+  }
+
+  createChart() {
+    const { data, width } = this.props
+
+    const xScale = d3
+      .scaleTime()
+      .domain([new Date("01/01/2017"), new Date("12/31/2017")])
+      .range([margin.left, width - margin.right]);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0,100])
+      .range([height - margin.bottom, margin.top]);
+
+      this.xAxis.scale(xScale);
+      d3.select(this.refs.xAxis).transition().call(this.xAxis);
+      this.yAxis.scale(yScale);
+      d3.select(this.refs.yAxis).transition().call(this.yAxis);
+
+    const colorExtent = d3.extent(data, d => d.avg).reverse();
+    const colorScale = d3
+      .scaleSequential()
+      .domain(colorExtent)
+      .interpolator(d3.interpolateRdYlBu);
+
+    d3.select(this.refs.chart)
+      .selectAll('rect')
+      .data(this.props.data)
+      .enter()
+      .append("rect")
+      .attr('y', d => yScale(d.high));
+
+    d3.select(this.refs.chart)
+      .selectAll("rect")
+      .data(this.props.data)
+      .exit()
+      .remove()
+
+    d3.select(this.refs.chart)
+      .selectAll("rect")
+      .transition()
+      .attr('x', d => xScale(d.date))
+      .transition()
+      .attr('y', d => yScale(d.high))
+      .attr('height', d => yScale(d.low) - yScale(d.high))
+      .attr('fill', d => colorScale(d.avg))
+      .attr('width', 2);
+  }
+
+  render() {
+    const { width } = this.props
+
+    return (
+      <svg width={width} height={height} ref="chart">
+        <g ref="xAxis" transform={`translate(0, ${height - margin.bottom})`} />
+        <g ref="yAxis" transform={`translate(${margin.left}, 0)`} />
+      </svg>
+    );
+  }
+}
+
+export default BarChart;
